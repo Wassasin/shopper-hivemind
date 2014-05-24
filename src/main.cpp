7762be85f4d00ac3c190c8d68101ded406ba818b
Wouter Geraedts <git@woutergeraedts.nl>
2014-05-24 16:38:07 +0200

    main: removed garbage

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,9 @@
 #include "util/convertor.h"
-#include "util/msgpack_reader.h"
 
 #include "offer.h"
 #include "transaction.h"
 #include "history.h"
 #include "typedefs.h"
-
-void test()
-{
-	using namespace Hivemind;
-
-
-
-
-
-	/*transaction t;
-	int result = 0, i = 0;
-	msgpack_reader<transaction> r("../data/transactions.msgpack.gz");
-	while(r.read(t))
-	{
-		result += t.id;
-
-		i++;
-		if(i == 500000)
-			break;
-	}
-
-	std::cout << i << std::endl;
-	std::cout << result << std::endl;*/
-}
 
 int main()
 {
@@ -43,8 +18,6 @@ int main()
 
 	Convertor::createDataset();
 
-	//convertor::csv_to_msgpack<transaction>("../data/transactions.csv.gz", "../data/transactions.msgpack.gz");
-	//test();
 	return 0;
 }
 
